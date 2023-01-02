6bec8af54e76b333dbb76babbaf2349d6d8adaad
Yusuf-R <y.abdulwasiu@gmail.com>
2023-01-02 04:58:24 +0100

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    alt = True
-    if i % 2 != 0:
-        alt = False
-    print("{}".format(chr(i) if alt else chr(i - 32)), end='')
+z_lower = 122
+A_upper = 89
+while 96 < z_lower and 64 < A_upper:
+    print("{}{}".format(chr(z_lower), chr(A_upper)), end="")
+    z_lower -= 2
+    A_upper -= 2
