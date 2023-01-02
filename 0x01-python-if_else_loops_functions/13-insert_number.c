adc99cc7a057a9e205d26320d745557a275c1e4f
Yusuf-R <y.abdulwasiu@gmail.com>
2023-01-02 03:13:28 +0100

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,38 +12,37 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node;
-	listint_t *curr, *prev;
+	listint_t *new_node, *curr, *prev;
 
 	new_node = prev = curr = NULL;
-	if (!*head)
+	if (!head)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
-	new_node->n = number;
-	new_node->next = NULL;
-
+	new_node->n = number, new_node->next = NULL;
 	curr = *head;
 	while (curr)
 	{
-		prev = curr;
-		curr = curr->next;
-		if (prev->n <= number && curr->n >= number)
+		prev = curr, curr = curr->next;
+		if (!*head)
 		{
-			new_node->next = curr;
-			prev->next = new_node;
-			return (new_node);
-		}
-		if (prev->n <= number && curr == NULL)
-		{
-			prev->next = new_node;
-			return (new_node);
-		}
-		if (number < 0)
-		{
-			new_node = (*head)->next;
 			*head = new_node;
+			return (new_node);
+		}
+		if (number <= 0)
+		{
+			new_node->next = *head, *head = new_node;
+			return (new_node);
+		}
+		if (number >= prev->n && curr && number <= curr->n)
+		{
+			new_node->next = curr, prev->next = new_node;
+			return (new_node);
+		}
+		if (number >= prev->n && curr == NULL)
+		{
+			prev->next = new_node;
 			return (new_node);
 		}
 	}
