ae61bc74fee4ad2725a79fb845812937b0b28255
Yusuf-R <y.abdulwasiu@gmail.com>
2023-01-03 02:52:29 +0100

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import calculator_1 as cal
 
 
 if __name__ == '__main__':
+    import calculator_1 as cal
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, cal.add(a, b)))
