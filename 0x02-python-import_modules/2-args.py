f760c3bef0e11f6874e7913184adf162ea0cf191
Yusuf-R <y.abdulwasiu@gmail.com>
2023-01-03 02:35:58 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    len = len(sys.argv) - 1
-    print("{} argument{}{}"
-          .format(len, "" if len == 1 else "s", "." if len == 0 else ":"))
-    i = 1
-    while i <= len:
-        print("{}: {}".format(i, sys.argv[i]))
-        i += 1
+import sys
+
+
+if __name__ == '__main__':
+    cmd_arg = sys.argv
+    cmd_len = len(cmd_arg)
+    if cmd_len < 2:
+        print("{} arguments.".format(cmd_len - 1))
+    elif cmd_len == 2:
+        print("{} argument:".format(cmd_len - 1))
+        for cmd_idx in range(1, cmd_len):
+            print("{}: {}".format(cmd_idx, cmd_arg[cmd_idx]))
+    elif cmd_len > 2:
+        print("{} arguments:".format(cmd_len - 1))
+        for cmd_idx in range(1, cmd_len):
+            print("{}: {}".format(cmd_idx, cmd_arg[cmd_idx]))
