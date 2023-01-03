9258ca7d2cb7a3f561f6d1e33f956d22a2ba649c
Yusuf-R <y.abdulwasiu@gmail.com>
2023-01-03 22:37:00 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == '__main__':
     import hidden_4
-    for l in dir(hidden_4):
-        if l[0:2] != "__":
-            print(l)
+    names_define = dir(hidden_4)
+    for pref_name in names_define:
+        if pref_name[0:2] != '__':
+            print(pref_name)
