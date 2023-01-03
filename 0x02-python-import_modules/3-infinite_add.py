242c86daaf80de94260f1e307062fae10917d650
Yusuf-R <y.abdulwasiu@gmail.com>
2023-01-03 23:45:26 +0100

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,7 @@ if __name__ == '__main__':
     import sys
     list_sum = 0
     cmd_argz = sys.argv
-    print(cmd_argz)
     list_len = len(cmd_argz)
-    print(list_len)
     for trav in range(1, list_len):
         list_sum += int(cmd_argz[trav])
-    print(list_sum)
+    print("{}".format(list_sum))
