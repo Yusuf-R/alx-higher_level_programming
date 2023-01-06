520e28c3d8026e18b4318e43a71f2a0d93040df2
Yusuf-R <y.abdulwasiu@gmail.com>
2023-01-06 03:04:27 +0100

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,23 +1,48 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len1 = len(tuple_a)
-    len2 = len(tuple_b)
+    n_tp1 = ()
+    n_tp2 = ()
+    l_tp1 = len(tuple_a)
+    l_tp2 = len(tuple_b)
 
-    if (len1 >= 2 and len2 >= 2):
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif (len1 == 1 and len2 == 1):
-        return (tuple_a[0] + tuple_b[0], 0)
-    elif (len1 == 0 and len2 == 0):
-        return (0, 0)
-    elif (len1 == 1 and len2 >= 2):
-        return (tuple_a[0] + tuple_b[0], tuple_b[1])
-    elif (len1 == 0 and len2 >= 2):
-        return (tuple_b[0], tuple_b[1])
-    elif (len1 >= 2 and len2 == 1):
-        return (tuple_a[0] + tuple_b[0], tuple_a[1])
-    elif (len1 >= 2 and len2 == 0):
-        return (tuple_a[0], tuple_a[1])
-    elif (len1 == 1 and len2 == 0):
-        return (tuple_a[0], 0)
-    elif (len1 == 0 and len2 == 1):
-        return (tuple_b[0], 0)
+    if l_tp1 >= 2:
+        for trv in tuple_a:
+            if trv == ():
+                trv = 0
+                n_tp1 += (trv,)
+                continue
+            else:
+                n_tp1 += (trv,)
+
+    elif l_tp1 == 1:
+        tuple_a += (0,)
+        n_tp1 = tuple_a
+
+    elif l_tp1 == 0:
+        tuple_a += (0,)
+        tuple_a += (0,)
+        n_tp1 = tuple_a
+
+    if l_tp2 >= 2:
+        for trv in tuple_b:
+            if trv == ():
+                trv = 0
+                n_tp2 += (trv,)
+                continue
+            else:
+                n_tp2 += (trv,)
+
+    elif l_tp2 == 1:
+        tuple_b += (0,)
+        n_tp2 = tuple_b
+
+    elif l_tp2 == 0:
+        tuple_b += (0,)
+        tuple_b += (0,)
+        n_tp2 = tuple_b
+
+    ln_tp1 = list(n_tp1)
+    ln_tp2 = list(n_tp2)
+    s_l = [ln_tp1[0] + ln_tp2[0], ln_tp1[1] + ln_tp2[1]]
+    s_l = tuple(s_l)
+    return s_l
