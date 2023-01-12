c383659ddeb031405bd99667f76491e0a4ce8961
Yusuf-R <y.abdulwasiu@gmail.com>
2023-01-12 12:14:19 +0100

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,50 +1,107 @@
 #include "lists.h"
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome.
- * @head: double pointer to start of linked list.
- * 
- * Return: 0 if not a palindrome, else return 1.
+  * odd_node_case -  Perform odd node action
+  *
+  * @mid: pointer to the mid point after split
+  * @adjuster: to set the end node to null
+  * @slow: the pointer from the initail traversing
+  *
+  * Return: a pointer to the start of the half node
+ */
+
+listint_t *odd_node_case(listint_t *mid, listint_t *adjuster, listint_t *slow)
+{
+	mid = slow->next;
+	while (adjuster)
+	{
+		if (adjuster->next == slow)
+		{
+			adjuster->next = NULL;
+			slow->next = NULL;
+			break;
+		}
+	adjuster = adjuster->next;
+	}
+	adjuster = NULL;
+	return (mid);
+}
+
+/**
+  * even_node_case -  Perform even node action
+  *
+  * @mid: pointer to the mid point after split
+  * @adjuster: to set the end node to null
+  * @slow: the pointer from the initail traversing
+  *
+  * Return: a pointer to the start of the half node
+ */
+
+listint_t *even_node_case(listint_t *mid, listint_t *adjuster, listint_t *slow)
+{
+	mid = slow->next;
+	while (adjuster)
+	{
+		if (adjuster->next == slow)
+		{
+			adjuster->next = NULL;
+			break;
+		}
+		adjuster = adjuster->next;
+	}
+	adjuster = NULL;
+	return (mid);
+}
+
+
+
+
+/**
+  * is_palindrome - a function to check if a list is a palindrome
+  *
+  * @head: the double pointer to the start of the node
+  *
+  * Return: return 1 or 0 depending if the True or False
+  *
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *node, *prev;
-	int failed = 0;
+	listint_t *fast, *slow, *mid, *prev, *next, *adjuster;
 
-	while (fast != NULL && fast->next != NULL)
+	if (!head)
+		return (0);
+	if (!(*head))
+		return (1);
+	fast = slow = *head;
+	mid = prev = next = adjuster = NULL;
+	if (fast == NULL || fast->next == NULL)
+		return (1);
+	while (fast && fast->next)
 	{
 		fast = fast->next->next;
 		slow = slow->next;
 	}
-	node = slow;
-	prev = NULL;
-	while (node)
+	if (fast)
 	{
-		fast = node->next;
-		node->next = prev;
-		prev = node;
-		node = fast;
+		fast = NULL, adjuster = *head;
+		mid = odd_node_case(mid, adjuster, slow);
 	}
-	fast = *head;
-	node = prev;
-	while (prev)
+	else if (!fast)
 	{
-		if (fast->n != prev->n)
-		{
-			failed = 1;
-			break;
-		}
-		fast = fast->next;
-		prev = prev->next;
+		adjuster = *head, mid = slow;
+		mid = even_node_case(mid, adjuster, slow);
 	}
-	prev = NULL;
-	while (node)
+	adjuster = NULL;
+	while (mid != NULL)
 	{
-		fast = node->next;
-		node->next = prev;
-		prev = node;
-		node = fast;
+		next = mid->next, mid->next = prev, prev = mid, mid = next;
 	}
-	return (!failed);
+	while (prev != NULL && *head != NULL)
+	{
+		if ((*head)->n != prev->n)
+			return (0);
+		prev = prev->next, *head = (*head)->next;
+	}
+	return (1);
 }
