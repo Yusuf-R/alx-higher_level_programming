a7eb1dcaa0b4f65b8ca191a9e3a79c1b09c4a2b5
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-08 03:11:03 +0100

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
-"""Square module - assigns size of the square and
-checks for type and value"""
+""" A module with the class definatoin of a square"""
 
 
 class Square:
-    """Defines a square with private instance attribute size"""
-    def __init__(self, size=0):
-        """assigns size of the square and checks for type and value"""
+    """
+    This is a blueprint class template for defining
+    an object square
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
+    Attributes
+    ----------
+    size (int) :    The sides of the square
+                    This is the size of it sides
+                    it is a private attribute
+    """
+
+    def __init__(self, size=0):
+        """
+        Constructor for all the necessary attributes for
+        the square object.
+
+        Asigns the size parameters base on type checks
+        and accepted values
+
+        Instance Parameters:
+        ----------
+            size (int) : The side of the square
+        """
+        if type(size) != int:
+            raise TypeError("Size must be an integer")
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
