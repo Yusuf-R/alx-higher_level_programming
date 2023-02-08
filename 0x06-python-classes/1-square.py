a6170547439dc723d1e310bc2d5d81d381d0b2b9
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-08 02:32:58 +0100

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
-"""Square module"""
+""" A module with the class definatoin of a square"""
 
 
 class Square:
-    """Defines a square"""
+    """
+    This is a blueprint class template for defining
+    an object square
+
+    Attributes
+    ----------
+    size (int) :    The sides of the square
+                    This is the size of it sides
+                    it is a private attribute
+    """
 
     def __init__(self, size):
+        """
+        Constructor for all the necessary attributes for
+        the square object.
+
+        Instance Parameters:
+        ----------
+            size (int) : The side of the square
+        """
+
         self.__size = size
