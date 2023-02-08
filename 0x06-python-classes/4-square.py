420f4c6866c74e1222ff59310b5f87a84ae50094
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-08 05:08:33 +0100

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,38 +1,88 @@
 #!/usr/bin/python3
-"""Square module - assigns size of the square and
-checks for type and value"""
+""" A module with the class definatoin of a square"""
 
 
 class Square:
-    """defines a square with private instance attribute size"""
-    def __init__(self, size=0):
-        """Constructor.
+    """
+    This is a blueprint class template for defining
+    an object square
 
-        Args:
-            size: Lenght of the side of a square
+    Attributes
+    ----------
+    size (int) :    The sides of the square
+                    This is the size of it sides
+                    it is a private attribute
+
+    Mehods:
+    ----------
+    area(self) : xcal the area of the square with the sides
+    """
+
+    def __init__(self, size=0):
         """
-        self.size = size
+        Constructor for all the necessary attributes for
+        the square object.
+
+        Asigns the size parameters base on type checks
+        and accepted values
+
+        Instance Parameters:
+        ----------
+            size (int) : The side of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
-        """Retrieves the value of size
+        """
+        This deocrator @property helps to set the function
+        to behave like an attributes
 
-        Raises:
-            TypeError: If size is not an int
-            ValueError; If size is less than 0
+        This helps to eliminate the () when calling it
+            Example:
+                my_square.size : This will return the value of size
+                rather than calling it as my_square.size()
+
+        Returns: the current value of the size of the square
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """property setter - sets the value of size"""
+        """
+        This decorator helps to set a new value to our size
 
-        if not isinstance(value, int):
+        It comes with an exception handling
+
+            ValueError
+            TypeError
+
+        Returns: the value of the new set side of the square
+        """
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        # if isinstance(int(size), int):
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def area(self):
-        """public instance method returns current sqr area"""
+        """
+        This calculates and returns the area of the square
+
+        Parameters
+        ----------
+        size(int) : The side of the square
+
+        Returns
+        ----------
+        The area of the square calculated
+        """
+
         return self.__size ** 2
