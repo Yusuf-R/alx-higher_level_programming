ed3c5064f1413eb5b1813e4daab05f8269ec890a
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-13 17:50:06 +0100

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-"""Module for say_my_name method."""
+"""
+This is a module that contains the function as defined
+
+Operations:
+    Exceptions:
+        TypeError : if the type is not a string
+
+    Returns:
+        The printed name having first and last name concat'd
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Method for printing first and last name.
+    """
+    This function retunrs the combined string of first name
+    last name and last name together
 
     Args:
-        first_name: first name string.
-        last_name: last name string.
+        first_name(str) : first name
+        last_name(str)  : last name
 
-    Raises:
-        TypeError: If first_name or last_name are not strings.
+    Return:
+        "first_name last_name"
+
+    Example:
+        first_name is Abdulwasiu
+        last_name is Yusuf
+        function returns = Abdulwasiu Yusuf
     """
+    te_1 = "first_name must be a string"
+    te_2 = "second_name must be a string"
     if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-
+        raise TypeError(te_1)
     if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-
-    print("My name is {:s} {:s}".format(first_name, last_name))
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/3-say_my_name.txt")
+        raise TypeError(te_2)
+    print("My name is {} {}".format(first_name, last_name))
