22453edd46a960b2e49b318578bd9e09678766ba
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-16 06:52:13 +0100

    101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,98 +1,167 @@
 #!/usr/bin/python3
-"""Module for lazy_matrix_mul method."""
+"""
+This a module that contain a functons for
+multiplying two given matrix
 
-import numpy
+Operation:
+    Given:
+        Matrix_A of rowA x colA
+        Matrix_B of rowB x colB
+    Condition:
+        colA == rowB
+    Result:
+        Matrix_N of rowA x colB
+
+
+It handles several excceptions:
+    Exceptions:
+        TypeError:
+            if the argument is not of type list
+            if the argument is not a list of a list
+            if any of the element is not an int or float
+            if each matrix rows are not the same
+        ValueError:
+            if the argument passed is an empty list
+            if both matrix does not satisfy the conditon
+                for matrix multiplicaton
+"""
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Multiplies one matrix by another.
-    Args:
-        m_a: the first matrix
-        m_b: the second matrix
-    Returns:
-        matrix: the product
-    Raises:
-        TypeError: If m_a or m_b are not lists.
-        TypeError: If m_a or m_b are not lists of lists.
-        ValueError: If m_a or m_b are empty lists/matrices.
-        TypeError: If m_a or m_b contain a non int/float.
-        TypeError: If m_a or m_b are not rectangular.
-        ValueError: If m_a or m_b can't be multiplied.
+    """
+    Function to multiply two Matix:
+
+        Args:
+            m_a: int/float - row_a x col_a
+            m_b: int/float - row_b x col_b
+
+        Result:
+            m_n: int/float - row_a x col_b
+
     """
 
-    '''
-    if not isinstance(m_a, list):
+    te_a_msg = "m_a should contain only integers or floats"
+    te_b_msg = "m_b should contain only integers or floats"
+
+    te_ra = "each row of m_a must be of the same size"
+    te_rb = "each row of m_b must be of the same size"
+
+    if matrix_type_check(m_a):
+        pass
+    else:
         raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
+
+    if matrix_type_check(m_b):
+        pass
+    else:
         raise TypeError("m_b must be a list")
-    '''
-    m_a_empty = False
-    m_b_empty = False
-    m_a_notrect = False
-    m_b_notrect = False
-    m_a_notnum = False
-    m_b_notnum = False
-    m_a_scalar = False
-    m_b_scalar = False
-    for row in m_a:
-        if not isinstance(row, list):
-            m_a_scalar = True
-        if len(row) != len(m_a[0]):
-            m_a_notrect = True
-        for num in row:
-            if not isinstance(num, (int, float)):
-                m_a_notnum = True
 
-    for row in m_b:
-        if not isinstance(row, list):
-            m_b_scalar = True
-        if len(row) != len(m_b[0]):
-            m_b_notrect = True
-        for num in row:
-            if not isinstance(num, (int, float)):
-                m_b_notnum = True
+    if is_matrix_list_of_list(m_a):
+        pass
+    else:
+        raise TypeError("m_a must be a list of lists")
 
-    '''
-    if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
-        raise ValueError("m_a can't be empty")
+    if is_matrix_list_of_list(m_b):
+        pass
+    else:
+        raise TypeError("m_b must be a list of lists")
 
-    if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0)x:
-        raise ValueError("m_b can't be empty")
-    '''
+    if is_matrix_empty(m_a):
+        pass
+    else:
+        raise TypeError("m_a can't be empty")
 
-    if m_a_scalar:
-        raise TypeError("Scalar operands are not allowed, use '*' instead")
+    if is_matrix_empty(m_b):
+        pass
+    else:
+        raise TypeError("m_b can't be empty")
 
-    if m_b_scalar:
-        raise TypeError("Scalar operands are not allowed, use '*' instead")
+    if is_list_of_list_strict_int_or_float(m_a) is False:
+        raise TypeError(te_a_msg)
 
-    if m_a_notnum:
-        raise TypeError("invalid data type for einsum")
+    if is_list_of_list_strict_int_or_float(m_b) is False:
+        raise TypeError(te_b_msg)
 
-    if m_b_notnum:
-        raise TypeError("invalid data type for einsum")
+    if is_list_list_rectangle(m_a) is False:
+        raise TypeError(te_ra)
 
-    if m_a_notrect:
-        raise ValueError("setting an array element with a sequence.")
+    if is_list_list_rectangle(m_b) is False:
+        raise TypeError(te_rb)
 
-    if m_b_notrect:
-        raise ValueError("setting an array element with a sequence.")
+    row_a = get_row(m_a)
+    col_a = get_col(m_a)
 
-    '''
-    if len(m_a[0]) != len(m_b):
+    row_b = get_row(m_b)
+    col_b = get_col(m_b)
+
+    if col_a != row_b:
         raise ValueError("m_a and m_b can't be multiplied")
 
-    res = [[] for i in range(len(m_a))]
+    new_matrix = np.dot(np.array(m_a), np.array(m_b))
+    return new_matrix
 
-    for i in range(len(m_a)):
-        for j in range(len(m_b[0])):
-            c = 0
-            for k in range(len(m_b)):
-                c += m_a[i][k] * m_b[k][j]
-            res[i].append(c)
-    '''
-    return numpy.matrix(m_a) * numpy.matrix(m_b)
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/101-lazy_matrix_mul.txt")
+def matrix_type_check(a_mx):
+    """Checks the type of the arg must be a type list"""
+    if type(a_mx) is list:
+        return True
+    else:
+        return False
+
+
+def is_matrix_list_of_list(a_mx):
+    """Ensure the arg is list of a list"""
+    check_list_list = all(isinstance(g, list) for g in a_mx)
+    if check_list_list:
+        return True
+    else:
+        return False
+
+
+def is_matrix_empty(a_mx):
+    """Checks if the arg is empty"""
+    if any(a_mx):
+        return True
+    else:
+        return False
+
+
+def is_list_of_list_strict_int_or_float(a_mx):
+    """Checks if all element is of type int or flaot"""
+    for i in range(len(a_mx)):
+        for j in range(len(a_mx[i])):
+            if type(a_mx[i][j]) in [int, float]:
+                pass
+            else:
+                return False
+
+
+def is_list_list_rectangle(a_mx):
+    """Checks if all the rows in each matrix are equal"""
+    for _, elem in enumerate(a_mx):
+        if len(elem) == len(a_mx[0]):
+            pass
+        else:
+            return False
+
+
+def get_row(a_mx):
+    """Returns the number of rows in the matrix"""
+    return len(a_mx)
+
+
+def get_col(a_mx):
+    """Returns the number of cols in the matrix"""
+    return len(a_mx[0])
+
+
+def new_matrix(r, c):
+    """Intantiate the new_matrix with zeroes"""
+    m = []
+    for _ in range(r):
+        z = []
+        for _ in range(c):
+            z.append(0)
+        m.append(z)
+    return m
