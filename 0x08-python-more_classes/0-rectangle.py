a701713d7f1f0406e736bc9c05b52189471b061e
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-17 09:31:14 +0100

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,37 @@
 #!/usr/bin/python3
-"""Rectangle module"""
+"""
+This is a module that contains the template for the class Rectangle
+
+Attributes:
+-----------
+    it contains none at the moment
+
+Methods:
+-----------
+    it contains none at the moment
+
+Exceptions
+-----------
+    it contains none at the moment
+
+"""
 
 
 class Rectangle:
-    """ Rectangle class"""
+    """
+    This is a template for the class Rectangle
+
+    Attributes:
+    -----------
+        it contains none at the moment
+
+    Methods:
+    -----------
+        it contains none at the moment
+
+    Exceptions
+    -----------
+        it contains none at the moment
+           
+    """
     pass
