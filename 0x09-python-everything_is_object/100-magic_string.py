b9cbb9b03558a85c6c03c84648d9f306c7ec5c90
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-19 01:48:41 +0100

    fix: :bug: fix the error for pycodestyle

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Returns a strig using memory of the previous call"""
+
+
 def magic_string(var=[]):
     """returns the string with the appended string"""
     var.append("BestSchool")
