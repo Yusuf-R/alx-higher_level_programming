dad3507561ff50323d71d14343ec655fcf834dcd
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-19 01:51:25 +0100

    docs: :memo: fix the word count error

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""Returns a strig using memory of the previous call"""
-
-
 def magic_string(var=[]):
-    """returns the string with the appended string"""
     var.append("BestSchool")
     return ", ".join(var)
