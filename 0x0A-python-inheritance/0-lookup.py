c103ae362e8d3e9f3d093ac69a09b727a41b0140
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-19 19:44:17 +0100

    docs: :memo: fix the error on pycodestyle

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,9 +4,10 @@ This module contains a function that prints
 out all the attributes and methods of an object
 """
 
+
 def lookup(obj):
     """
     This function returns the list of all available
     attributes and mthods of an object
     """
-    return(dir(obj))
+    return (dir(obj))
