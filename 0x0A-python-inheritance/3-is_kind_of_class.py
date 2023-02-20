cdeeb3318e744562e044ee2d36af1fc98cf9e28c
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-20 18:28:15 +0100

    feat: :sparkles: a fuction that checks if an obj is direct or indirect instane of a class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-"""Module containing is_kind_of_class method"""
+"""
+This module has a function that checks if an object
+is a direct or indirect instance of a given class
+"""
 
 
 def is_kind_of_class(obj, a_class):
-    """returns True if the object is an instance of, or if the
-    object is an instance of a class that inherited from
-    the specified class ; otherwise False """
-    return isinstance(obj, a_class)
+    """
+    This checks if the obj is a class of a_class
+    or a subclass of a_claas
+
+    Returns:
+        True: if obj direct instance or indirect instacne
+        of a_class
+        Fase: otherwise
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
