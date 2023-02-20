948962e899d55c895463a79d48986a9bc38da581
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-20 22:21:40 +0100

    feat: :sparkles: returns true if the object is strictly froma an inherited class
    This code works in this manner.
    It will only return True if the object is an instance
    of a class that inherited another class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-"""Module containing inherits_from method"""
+"""
+This module has a function that checks if an object
+is an instance of anothoer class strictly
+"""
 
 
 def inherits_from(obj, a_class):
-    """returns True if the object is an instance of a class that
-    inherited (directly or indirectly) from the specified class;
-    otherwise False"""
-    return isinstance(obj, a_class) and type(obj) != a_class
+    """
+    This checks if the obj is an instance of another class
+    and not the direct class
+
+    Returns:
+        True: if obj is an instance of another class
+        Fase: if the obj is direct instace of the class
+    """
+    obj_cls = type(obj)
+    obj_mro = obj_cls.mro()
+    if a_class in obj_mro and obj_mro.index(a_class) != 0:
+        return True
+    else:
+        return False
