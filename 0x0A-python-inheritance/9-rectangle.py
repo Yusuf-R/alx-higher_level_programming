b931e011712a5457b7bede2ba4441ae2d86502b7
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-21 03:02:16 +0100

    feat: :sparkles: implment a method to calculate the area of the rectangle and also a string representation of the object

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
-"""Rectangle class Module"""
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""
+This module contains a class that inherits from a superclass
+as defined
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class"""
+    """
+    This class will inherit from Base_Geometry class
+    Private Instance Attributes:
+        width:  int
+        height: int
+    """
     def __init__(self, width, height):
-        """Initilize rectangle method"""
-        self.integer_validator("width", width)
+        """
+        Constructor object instance of the Rectangle class
+
+        Parameters
+        ----------
+        width:  int
+                width of the rectangle
+
+        height: int
+                height of the rectangle
+        """
+        super().integer_validator(name="width", value=width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator(name="height", value=height)
         self.__height = height
 
     def area(self):
-        """Method that returns area of rectangle"""
-        return self.__width * self.__height
+        area =  self.__width * self.__height
+        return area
 
     def __str__(self):
-        """Returns a string"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        rec_name = self.__class__.__name__
+        return "[{}] {}/{}".format(rec_name, self.__width, self.__height)
