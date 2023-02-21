69dbed9b38aa99e05f7c9e2d63f849b6e950d023
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-21 23:33:35 +0100

    feat: :sparkles: a function that does serialisation of a python object
    This methods encodes a python object to json object

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-"""Module with to_json_string method"""
+"""
+This module contains a function that serialize a python object"
+
+In here we use the dumps() since we are not dealing with a file
+"""
 import json
 
 
 def to_json_string(my_obj):
-    """Method that returns the JSON representation of an object"""
-    return json.dumps(my_obj)
+    """
+    This function serialises a python object to a json
+        This process is called Encoding
+    """
+    obj_json = json.dumps(my_obj)
+    return obj_json
