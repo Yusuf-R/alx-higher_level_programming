498e11e9e6d446220b35408c1a1ce089df4faff8
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-21 02:24:04 +0100

    feat: :sparkles: a subclass that inherits from superclass

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,34 @@
-#!/usr/bin/python3
-"""Rectangle class Module"""
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+#!/user/bin/python3
+"""
+This module contains a class that inherits from a superclass
+as defined
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class"""
+    """
+    This class will inherit from Base_Geometry class
+    Private Instance Attributes:
+        width:  int
+        height: int
+    """
     def __init__(self, width, height):
-        """Initilize rectangle method"""
-        self.integer_validator("width", width)
+        """
+        Constructor object instance of the Rectangle class
+
+        Parameters
+        ----------
+        width:  int
+                width of the rectangle
+
+        height: int
+                height of the rectangle
+        """
+        super().integer_validator(name="width", value=width)
+        super().integer_validator(name="height", value=height)
+
         self.__width = width
-        self.integer_validator("height", height)
         self.__height = height
