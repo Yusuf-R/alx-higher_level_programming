7daa69f600e85375cae83f11db67c125006d5613
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-22 00:20:03 +0100

    feat: :sparkles: a function that serialise a py_obj to json_obj
    This funciton performs encoding and saves the json to the specified file
    The function uses the load() method of json library

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""Module with save_to_json_file method"""
+"""
+This module contains a function that serialize
+a python object to a json obj and saves it in a file
+
+In here we use the dump() function
+"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Method that writes an Object to a text file using JSON rep"""
-    with open(filename, "w", encoding="utf-8") as f:
-        f.write(json.dumps(my_obj))
+    """
+    This function serialises a python object to json object
+        This process is called Encoding
+    """
+    with open(filename, "w", encoding="utf-8") as fd:
+        json.dump(my_obj, fd)
