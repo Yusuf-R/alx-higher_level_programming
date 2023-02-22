786cd082c3d4c3ff4fe4748061c1d5c5a3c7c8c0
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-22 05:28:44 +0100

    feat: :sparkles: a script that adds item and saves it to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3
-"""Module for a script that adds all arguments to a Python list"""
-import json
+"""
+Script that adds all arguments to a Python
+list, and saves them to a file
+    Description:
+        We have a file (if not, we create it)
+        This file is named add_item.json in our system
+
+        We want to achieve three things:
+            Load this file:
+                --we check if this file exist using the
+                os.path module
+                --if it is successful, we call the load function
+                --then we collec all its content with our list varible
+                -- Done
+            Collect more data inbetween:
+                -- more data could be stored in our list
+                -- we use the sys.argv module to do that
+                -- if our argv len is > 1, we append the
+                    command line argument into our list
+            Save
+                -- we call our save function to save
+                    all our python list object in to the filname
+
+"""
+from os import path
 from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file_name = "add_item.json"
+if __name__ == "__main__":
+    save_file = "add_item.json"
+    python_list = []
 
-# can also use os.path.exists(file) for try and except
-# be sure to import os.path
-try:
-    json_list = load_from_json_file(file_name)
-except:
-    json_list = []
-for item in argv[1:]:
-    json_list.append(item)
+    if path.exists(save_file) and path.isfile(save_file):
+        python_list = load_from_json_file(save_file)
 
-save_to_json_file(json_list, file_name)
+    if len(argv) > 1:
+        for argz in argv[1:]:
+            python_list.append(argz)
+
+    save_to_json_file(python_list, save_file)
