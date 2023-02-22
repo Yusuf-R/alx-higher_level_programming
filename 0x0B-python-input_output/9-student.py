a7349717133a73e5b58052937ae0230a10209b49
Yusuf-R <y.abdulwasiu@gmail.com>
2023-02-22 07:15:30 +0100

    feat: :sparkles: a class that defins a student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Student class Module"""
+"""This module contains a class that defines a student"""
 
 
-class Student():
-    """Student class"""
+class Student:
+    """This class is a template for its object instances"""
     def __init__(self, first_name, last_name, age):
-        """Initializes Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Gets dict"""
-        return self.__dict__.copy()
+        """return the attribute in a dict form"""
+        return self.__dict__
