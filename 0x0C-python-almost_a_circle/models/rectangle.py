f147cf43101fe4a34ea30323d47d776062bc44a4
Yusuf-R <y.abdulwasiu@gmail.com>
2023-03-15 17:14:24 +0100

    feat: :sparkles: add an input validator at the instantiation of the class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,100 +1,105 @@
 #!/usr/bin/python3
-"""Rectangle class module"""
+"""
+This module contains a class that inherits from a superclass
+"""
+
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class"""
+    """
+    Class Rectangle that inherits from Base
+    Private instance attributes:
+        width: int
+        height: int
+        x: int
+        y: int
+        """
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Constructor"""
+        """
+        Instantiation with width, height, x, y, id
+        """
+
+        Rectangle.input_validator("width", width)
+        Rectangle.input_validator("heigth", height)
+        Rectangle.input_validator("x", x)
+        Rectangle.input_validator("y", y)
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    def validate(self, name, value):
-        """validates all values"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if (name == "width" or name == "height") and value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-        elif (name == "x" or name == "y") and value < 0:
-            raise ValueError("{} must be >= 0".format(name))
-
-    @property
-    def x(self):
-        """rectangle's x pos"""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        self.validate("x", value)
-        self.__x = value
-
-    @property
-    def y(self):
-        """rectangle's y pos"""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        self.validate("y", value)
-        self.__y = value
 
     @property
     def width(self):
-        """rectangle's width"""
+        """
+        width getter
+        """
         return self.__width
 
     @width.setter
-    def width(self, value):
-        self.validate("width", value)
-        self.__width = value
+    def width(self, data):
+        """
+        width setter
+        """
+        Rectangle.input_validator("width", data)
+        self.__width = data
 
     @property
     def height(self):
-        """rectangle's height"""
+        """
+        height getter
+        """
         return self.__height
 
     @height.setter
-    def height(self, value):
-        self.validate("height", value)
-        self.__height = value
+    def height(self, data):
+        """
+        height setter
+        """
+        Rectangle.input_validator("height", data)
+        self.__height = data
 
-    def area(self):
-        """returns area of this rectangle"""
-        return self.width * self.height
+    @property
+    def x(self):
+        """
+        x getter
+        """
+        return self.__x
 
-    def display(self):
-        """Prints rectangle instance with the character #"""
-        rec_s = "\n" * self.y + (" " * self.x + "#" * self.width +
-                                 "\n") * self.height
-        print(rec_s, end="")
+    @x.setter
+    def x(self, data):
+        """
+        x setter
+        """
+        Rectangle.input_validator("x", data)
+        self.__x = data
 
-    def __str__(self):
-        """returns a string rep of this rectangle"""
-        return "[{}] ({}) {}/{} - {}/{}"\
-            .format(type(self).__name__, self.id, self.x, self.y,
-                    self.width, self.height)
+    @property
+    def y(self):
+        """
+        y getter
+        """
+        return self.__y
 
-    def __updArgsKwarg(self, id=None, width=None, height=None, x=None, y=None):
-        """Internal method for updating the square"""
-        args = locals()
-        for k in args.keys():
-            if args[k] is not None and k != "self" and k == "id":
-                self.__dict__[k] = args[k]
-            elif args[k] is not None and k != "self":
-                self.__dict__["_" + type(self).__name__ + "__" + k] = args[k]
+    @y.setter
+    def y(self, data):
+        """
+        y setter
+        """
+        Rectangle.input_validator("y", data)
+        self.__y = data
 
-    def update(self, *args, **kwargs):
-        """Updates the square"""
-        if args:
-            self.__updArgsKwarg(*args)
-        elif kwargs:
-            self.__updArgsKwarg(**kwargs)
-
-    def to_dictionary(self):
-        """returns a dictionary rep of this rectangle"""
-        return {"id": self.id, "width": self.width, "height": self.height,
-                "x": self.x, "y": self.y}
+    @staticmethod
+    def input_validator(data_name, data_value):
+        """
+        Validates all the inputs data
+        """
+        if type(data_value) is not int:
+            raise TypeError("{} must be an integer".format(data_name))
+        if (data_name == "width" or data_name == "heigth") and data_value <= 0:
+            raise ValueError("{} must be > 0".format(data_name))
+        if (data_name == "x" or data_name == "y") and data_value < 0:
+            raise ValueError("{} must be >= 0".format(data_name))
