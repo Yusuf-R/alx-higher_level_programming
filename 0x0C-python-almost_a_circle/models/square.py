32fbd42e436939b5d7b4f5f661903e14f0fd724c
Yusuf-R <y.abdulwasiu@gmail.com>
2023-03-16 22:51:20 +0100

    feat: :sparkles: a class defination that inherits from a super class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,49 +1,32 @@
 #!/usr/bin/python3
-"""Square class module"""
+"""
+This is a module for describing the Square template
+    it will inherits from the class Rectangle
+"""
+
 from models.rectangle import Rectangle
 
-
 class Square(Rectangle):
-    """Square class"""
+    """
+    This is a class Square that inherits from the class Rectangle
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """square constructor"""
-        super().__init__(width=size, height=size, x=x, y=y, id=id)
+        """
+        This is a constructor for the class Square
+        """
+        super().__init__(size, size, x, y, id)
+        Rectangle.input_validator("size", size)
+        self.size = size
+        Rectangle.input_validator("x", x)
+        self.x = x
+        Rectangle.input_validator("y", y)
+        self.y = y
 
     def __str__(self):
-        """Returns a string rep of this square"""
-        return "[{}] ({}) {}/{} - {}"\
-            .format(type(self).__name__, self.id, self.x, self.y,
-                    self.height)
+        """
+        This is a string representation of the class Square
+        """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
+                                                          self.y, self.size)
 
-    @property
-    def size(self):
-        """This square size property"""
-        return self.width
 
-    @size.setter
-    def size(self, value):
-        self.width = value
-        self.height = value
-
-    def __update(self, id=None, size=None, x=None, y=None):
-        """internal method for updating square"""
-        if id is not None:
-            self.id = id
-        if size is not None:
-            self.size = size
-        if x is not None:
-            self.x = x
-        if y is not None:
-            self.y = y
-
-    def update(self, *args, **kwargs):
-        """Updates the square"""
-        if args:
-            self.__update(*args)
-        elif kwargs:
-            self.__update(**kwargs)
-
-    def to_dictionary(self):
-        """returns a dictionary rep of this square"""
-        return {"id": self.id, "size": self.size, "x": self.x,
-                "y": self.y}
