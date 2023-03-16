021ab5530ddab048a304dfd6e53a364d336c2b1f
Yusuf-R <y.abdulwasiu@gmail.com>
2023-03-16 01:41:28 +0100

    feat: :sparkles: implement args for variable assignment

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -143,3 +143,18 @@ class Rectangle(Base):
         """
         return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def updat(self, *args):
+        """
+        Updates the attributes of a rectangle
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) >= 5:
+            self.__y = args[4]
