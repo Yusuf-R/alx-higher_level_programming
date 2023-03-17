713b2b21f68d7ee5c6f8ddea4762650a1735782f
Yusuf-R <y.abdulwasiu@gmail.com>
2023-03-17 01:11:44 +0100

    feat: :sparkles: perform json serialisation of a python object

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is  module that contian the base definaton of a class Base"""
-
+import json
 
 class Base:
     """This is a base class"""
@@ -18,3 +18,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """This is a static method that returns the JSON string representation
+        of list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
