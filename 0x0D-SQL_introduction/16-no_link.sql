bd28ccb0e92bbc9e5c3911aaa80b6d42730baf7e
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-02 07:11:15 +0100

    feat: :sparkles: a script that lists all records of the table based on some tigt nitted criteria

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,8 @@
--- lists all records of second_table
--- avoid rows without name val
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+-- a script that lists all records of the table second_table of the database
+-- Don’t list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+SELECT score, name
+FROM second_table 
+where name is not NULL 
+order by score desc;
