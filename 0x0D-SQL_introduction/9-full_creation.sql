87fb4b70ca0a401447ebccb3809c1746aac68339
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-02 04:03:08 +0100

    feat: :sparkles: create a new table and update it with the right records

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,12 @@
--- creates a table 'second_table' in db hbtn_0c_0
--- full creation
-CREATE TABLE IF NOT EXISTS second_table(
-	id INT,
-	name VARCHAR(256),
-	score INT);
-INSERT INTO second_table(id, name, score)
-VALUES	(1, "John", 10),
-	(2, "Alex", 3),
-	(3, "Bob", 14),
-	(4, "George", 8);
+-- a script that creates a table second_table
+-- the following records and attirbutes will be populated
+-- id INT
+-- name VARCHAR(256)
+-- score INT
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) 
+VALUES 
+(1, "John", 10),
+(2, "Alex", 3),	
+(3, "Bob", 14),
+(4, "George", 8);
