14c8b616005c5172782df3fd1bb206ffb7b7eb84
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-02 04:23:08 +0100

    feat: :sparkles: display the table base on ascending descending order of the specified fields

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,4 @@
--- lists all records of second_table
--- displays score and name
--- Descending order
-SELECT score, name FROM second_table
-ORDER BY score DESC
+-- script that lists all records of the table second_table
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
