f245410c641547085d0a228a42e7590427ad8c67
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-02 08:30:06 +0100

    feat: :sparkles: a script that displays the average temperature
    a script that displays the average temperature (Fahrenheit) by city ordered by temperature
    (descending).

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,6 @@
--- import dump into hbtn_0c_0
--- displays avg temp by city ordered by temp (desc)
-SELECT city, AVG(value) avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+-- a script that displays the average temperature of cities
+-- ordered by temperature (descending).
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
