7e60f69fec9dabbf7b49cf70466b0a2b2a2fae1e
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-02 02:53:40 +0100

    feat: :sparkles: show the table details without using DESCRIBE

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
--- prints the full desc of the table 'first_table' from db hbtn_0c_0
--- in MYSQL server
--- Not allowed to used DESCRIBE or EXPLAIN
-SHOW CREATE TABLE first_table;
+-- prints the full description of the table
+-- without using DESCRIBE or EXPLAIN
+SELECT * FROM first_table;
