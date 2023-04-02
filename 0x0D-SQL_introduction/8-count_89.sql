c1419c4d0e5acbba0bd453f77ee0753ecc82a376
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-02 03:38:58 +0100

    feat: :sparkles: count all the instance where id = 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,2 @@
--- displays number of records with id = 89 in first_table
--- db name - hbtn_0c_0
-SELECT COUNT(id) FROM first_table WHERE id = 89; 
+-- a script that displays the number of records with id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
