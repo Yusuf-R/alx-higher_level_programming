2056ad8e9339eb7a6fdc5e55d5e1cac32bcaceb6
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-02 04:56:22 +0100

    feat: :sparkles: updates the score of Bob to 10 in the table

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,4 @@
--- updates the score of Bob to 10
--- Not allowed to use id val, use name field
+-- updates the score of Bob to 10 in the table
 UPDATE second_table
 SET score = 10
 WHERE name = "Bob";
