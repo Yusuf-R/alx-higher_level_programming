99b1da2ec885e2652520e467a1e21ae27499f3ae
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-03 06:53:12 +0100

    feat: :sparkles: a script that creates the database hbtn_0d_usa and the table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,7 @@
--- creates db hbtn_9d_usa
-
+--  a script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- create table states
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	name VARCHAR(256) NOT NULL);	
+	name VARCHAR(256) NOT NULL
+);
