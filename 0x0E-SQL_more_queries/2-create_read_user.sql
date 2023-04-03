1724f4373b9c6ca6af632670faeab1f9d7527b17
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-03 04:01:11 +0100

    fix: :bug: correct error in the user field

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,11 @@
 -- creates a user if not exist
 -- grant the user only select privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+
+CREATE USER 
+IF NOT EXISTS 'user_0d_2'@'localhost' 
+IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT 
+ON hbtn_0d_2.* 
+TO 'user_0d_2'@'localhost';
