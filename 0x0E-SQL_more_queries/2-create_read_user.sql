85a172310bcc0202b042b72f568777f89574cb03
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-03 03:57:14 +0100

    feat: :sparkles: create second database and second user with only select privilege

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,7 @@
--- creates database hbtn_0d_2
+-- a script that 
+-- creates the database if not exists
+-- creates a user if not exist
+-- grant the user only select privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- create user user_0d_2
-CREATE USER IF NOT EXISTS user_0d_2@localhost
-IDENTIFIED BY "user_0d_2_pwd";
-
--- grant privileges
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
