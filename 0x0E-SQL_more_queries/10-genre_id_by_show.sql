f04c3f57bfab4245ff707cc386f9ca548dadcb51
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-03 23:37:40 +0100

    feat: :sparkles: a script that lists all shows contained in hbtn_0d_tvshows database
    Each record should display: tv_shows.title - tv_show_genres.genre_id
    
    BREAKING CHANGE: The database name will be passed as an argument of the mysql command

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,9 @@
--- lists all shows contained in hbtn_0d_tvshows
-
+-- A script that lists all shows contained in hbtn_0d_tvshows 
+-- Have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows 
-INNER JOIN tv_show_genres
-WHERE tv_shows.id  = tv_show_genres.show_id 
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
