8fd9c26f84f37a4633f9e5e834405b7e0fa01b07
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-03 23:44:19 +0100

    feat: :sparkles: a script that lists all shows contained in the database
    Write a script that lists all shows contained in the database
    
    Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
    
    If a show doesn’t have a genre, display NULL

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,10 @@
--- list all shows contained in the db
-
+-- A script that lists all shows contained in hbtn_0d_tvshows 
+-- Have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- if a show doesn't have a genre display null
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows 
+FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_shows.id  = tv_show_genres.show_id 
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
