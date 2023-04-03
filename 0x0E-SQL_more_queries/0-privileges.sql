5cf33b493489967d89b292e9b1024d2b66adb5df
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-03 02:25:33 +0100

    feat: :sparkles: a script that lists all privileges of the MySQL users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- list all privileges of the MySQL users
-
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- a script that lists all privileges of the MySQL users
+-- the users are user_0d_1 and user_0d_2 on the server
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
