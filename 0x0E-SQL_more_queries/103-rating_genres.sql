1a4cd920e37683b0d357e9c4a1ad01fa650aa31c
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-04 12:57:25 +0100

    feat: :sparkles: a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,9 +1,11 @@
--- all genres from hbtn_0d_tvshows_rate by their rating
-
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) rating 
-FROM tv_genres 
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
-INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id 
-INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id  
-GROUP BY tv_genres.name 
+-- a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating
+SELECT tg.name, SUM(ts.rate) as rating
+FROM tv_genres tg
+JOIN tv_show_genres tsg
+ON tg.id = tsg.genre_id
+JOIN tv_shows ts
+ON ts.id = tsg.show_id
+JOIN tv_show_ratings tsr
+ON ts.id = tsr.show_id
+GROUP BY tg.name
 ORDER BY rating DESC;
