a5b347c564d8b04cb2cccf2ca6d587e503d4ceb4
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-04 10:04:28 +0100

    refactor: :recycle: making sql presentaion compliant

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -5,7 +5,8 @@ FROM tv_genres tg
 WHERE tg.id NOT IN (
   SELECT tsg.genre_id
   FROM tv_show_genres tsg
-  JOIN tv_shows ts ON tsg.show_id = ts.id
+  JOIN tv_shows ts 
+  ON tsg.show_id = ts.id
   WHERE ts.title = 'Dexter'
 )
 ORDER BY tg.name ASC;
