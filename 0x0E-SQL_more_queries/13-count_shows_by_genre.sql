e19955d9a34cc6147f21da56d559c273c89f13d3
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-04 03:21:31 +0100

    fix: :bug: reduce the number of comments

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,5 @@
 -- a script that lists all genres from hbtn_0d_tvshows
 -- and displays the number of shows linked to each.
--- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
 -- First column must be called genre
 -- Second column must be called number_of_shows
 -- Don’t display a genre that doesn’t have any shows linked
