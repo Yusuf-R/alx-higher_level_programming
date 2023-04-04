ebcb21b99b15ac6064db86141e1b0ce60aec94ce
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-04 06:26:01 +0100

    feat: :sparkles: display all the genre associated wiht the movie title "Dexter"

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,11 @@
--- lists all genre of the Dexter show
-
-SELECT tv_genres.name 
-FROM tv_genres 
-INNER JOIN tv_show_genres 
-ON tv_show_genres.genre_id = tv_genres.id
-INNER JOIN tv_shows 
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres.name;
+-- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter 
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+SELECT tg.name
+FROM tv_shows ts
+JOIN tv_show_genres tsg
+ON ts.id = tsg.show_id
+JOIN tv_genres tg
+ON tg.id = tsg.genre_id
+WHERE ts.title = 'Dexter'
+ORDER BY tg.name ASC;
