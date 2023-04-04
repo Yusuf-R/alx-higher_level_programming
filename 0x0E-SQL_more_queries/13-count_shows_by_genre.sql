53fbfdd269d3f07879efc1b3f244ba73f9027fe2
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-04 03:08:09 +0100

    feat: :sparkles: list the genre and display it's frequency

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,13 @@
--- lists all genres and displays no. of shows linked to each
-
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows 
-FROM tv_genres
-INNER JOIN tv_show_genres 
-WHERE tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
-ORDER BY number_of_shows DESC;
+-- a script that lists all genres from hbtn_0d_tvshows
+-- and displays the number of shows linked to each.
+-- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+-- First column must be called genre
+-- Second column must be called number_of_shows
+-- Don’t display a genre that doesn’t have any shows linked
+-- Results must be sorted in descending order by the number of shows linked
+SELECT tg.name, COUNT(*) as number_of_shows
+FROM tv_genres tg
+JOIN tv_show_genres tsg
+ON tg.id = tsg.genre_id
+GROUP BY tsg.genre_id
+ORDER BY number_of_shows DESC
