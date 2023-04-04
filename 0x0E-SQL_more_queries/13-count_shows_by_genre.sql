2ca6496f03d7784890de29b61899f1e5eb05299f
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-04 03:23:57 +0100

    fix: :bug: ensure all keywords are in UpperCase

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,7 +4,7 @@
 -- Second column must be called number_of_shows
 -- Don’t display a genre that doesn’t have any shows linked
 -- Results must be sorted in descending order by the number of shows linked
-SELECT tg.name as genre, COUNT(*) as number_of_shows
+SELECT tg.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres tg
 JOIN tv_show_genres tsg
 ON tg.id = tsg.genre_id
