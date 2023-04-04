214682109a159390901ba465832a80ca36bd01b9
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-04 11:52:54 +0100

    feat: :sparkles: a script that lists all shows from hbtn_0d_tvshows_rate by their rating.

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
--- all shows from hbtn_0d_tvshows_rate by their rating
-
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
-FROM tv_shows
-INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title 
-ORDER BY rating DESC
+-- a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT ts.title, SUM(tsr.rate) AS rate
+FROM tv_shows ts
+JOIN tv_show_ratings tsr
+ON ts.id = tsr.show_id
+GROUP BY ts.id
+ORDER BY rate DESC;
