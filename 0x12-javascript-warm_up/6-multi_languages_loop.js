990b93a519d6f5c22afe1a18d609dc75c1fa6178
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-15 19:53:57 +0100

    feat: :sparkles: printing the content of an array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
-while (i < langs.length) {
-  console.log(langs[i]);
-  i += 1;
+const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i in msg) {
+  console.log(msg[i]);
 }
