d9ace648bd4095adf28dd1439b4452c44d6dfbfc
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-15 12:24:58 +0100

    feat: :sparkles: displaying only the first agrument passed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
-if (process.argv[2]) {
-  console.log(process.argv[2]);
-} else {
+const sArgv = require('process');
+let count = 0;
+
+for (const i of sArgv.argv) {
+  count++;
+}
+
+if (count < 3) {
   console.log('No argument');
+} else {
+  const arg2 = sArgv.argv[2];
+  console.log(arg2);
 }
