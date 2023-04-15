6d7c3895e73dc0719ef19109e3dd7d6a9f63573a
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-15 09:25:26 +0100

    feat: :sparkles: using system argument for our js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const len = process.argv.length;
-if (len <= 2) {
+import { argv } from 'node:process';
+if (argv.length < 3) {
   console.log('No argument');
-} else if (len === 3) {
-  console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log('Argument found');
 }
