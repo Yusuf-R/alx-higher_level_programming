084e6b8d88ef73d5a9d3e4ea70348f5b0a96385b
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-15 09:39:19 +0100

    feat: :sparkles: different output for different number of passed parameters

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const sArgv = require('process');
 if (sArgv.argv.length < 3) {
   console.log('No argument');
-} else {
+} else if (sArgv.argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
