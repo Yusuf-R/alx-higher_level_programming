26fe883af977385fb8b5e5656b52c67709053997
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-15 22:58:55 +0100

    feat: :sparkles: a script that prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const x = Number.parseInt(process.argv[2], 10);
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
+const sysArgv = require('process');
+const msg = 'C is fun';
+if (sysArgv.argv[2]) {
+  const count = sysArgv.argv[2];
+  for (let i = 0; i < count; i++) {
+    console.log(msg);
   }
+} else if (!sysArgv.argv[2]) {
+  console.log('Missing number of occurrences');
 }
