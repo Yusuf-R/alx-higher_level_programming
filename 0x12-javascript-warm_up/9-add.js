f203ac7c61a2ceff427c617982fb58abd3af4604
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-16 06:38:47 +0100

    feat: :sparkles: add two numbers using a function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-const a = Number.parseInt(process.argv[2], 10);
-const b = Number.parseInt(process.argv[3], 10);
-
 function add (a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    console.log('NaN');
-  } else {
-    console.log(a + b);
-  }
+  return (a + b);
 }
-add(a, b);
+
+const sysArg = require('process');
+const n1 = sysArg.argv[2];
+const n2 = sysArg.argv[3];
+
+const val1 = Number.parseInt(n1, 10);
+const val2 = Number.parseInt(n2, 10);
+
+const result = add(val1, val2);
+console.log(result);
