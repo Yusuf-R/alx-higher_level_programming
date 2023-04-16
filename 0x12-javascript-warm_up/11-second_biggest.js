b4870b8c33eaf7b54c2f5d9a39e11e4cc3d5db40
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-17 00:40:07 +0100

    feat: :sparkles: a script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,53 @@
 #!/usr/bin/node
-let largest = 0;
-let secondLargest = 0;
-
-if (process.argv.length < 4) {
+const len_vec = process.argv.length
+let arr_input = []
+if (len_vec < 4) {
   console.log(0);
 } else {
-  for (let i = 0; i < process.argv.length; i++) {
-    const num = Number.parseInt(process.argv[i]);
-    if (num > largest) {
-      secondLargest = largest;
-      largest = num;
-    } else if (num > secondLargest && num < largest) {
-      secondLargest = num;
+  for (let i = 2; i < len_vec; i++) {
+    if (isNaN(process.argv[i])) {
+      console.log(0);
+      break;
+    } else {
+      arr_input.push(parseInt(process.argv[i]));
     }
   }
-  console.log(secondLargest);
+  const arrLen = arr_input.length;
+  let m2 = 0;
+  if (arrLen === 2) {
+    m2 = arr_input[0];
+    if (arr_input[0] > arr_input[1]) {
+      m2 = arr_input[1];
+    } else {
+      m2 = m2;
+    }
+  } else {
+    i = 0;
+    let max = 0;
+    let idx = 0;
+    while (i < arrLen) {
+      if (max < arr_input[i]) {
+        max = arr_input[i];
+        idx = i;
+      } else {
+        max = max;
+        idx = idx;
+      }
+      i++; 
+    }
+    i = -1;
+    while (i < arrLen) {
+      i++;
+      if (i === idx) {
+        continue;
+      } else {
+        if (m2 < arr_input[i]) {
+          m2 = arr_input[i];
+        } else {
+          m2 = m2;
+          }
+        }
+      }
+    }
+  console.log(m2);
 }
