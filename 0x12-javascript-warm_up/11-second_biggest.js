22a08606d53c161e475f086af21fd4cd099fd58f
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-17 01:21:03 +0100

    feat: :sparkles: semi-standard compliant

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,39 +1,35 @@
 #!/usr/bin/node
-const len_vec = process.argv.length
-let arr_input = []
-if (len_vec < 4) {
+const lenVec = process.argv.length;
+const arrInput = [];
+let i = 0;
+if (lenVec < 4) {
   console.log(0);
 } else {
-  for (let i = 2; i < len_vec; i++) {
+  for (i = 2; i < lenVec; i++) {
     if (isNaN(process.argv[i])) {
       console.log(0);
       break;
     } else {
-      arr_input.push(parseInt(process.argv[i]));
+      arrInput.push(parseInt(process.argv[i]));
     }
   }
-  const arrLen = arr_input.length;
+  const arrLen = arrInput.length;
   let m2 = 0;
   if (arrLen === 2) {
-    m2 = arr_input[0];
-    if (arr_input[0] > arr_input[1]) {
-      m2 = arr_input[1];
-    } else {
-      m2 = m2;
+    m2 = arrInput[0];
+    if (arrInput[0] > arrInput[1]) {
+      m2 = arrInput[1];
     }
   } else {
     i = 0;
     let max = 0;
     let idx = 0;
     while (i < arrLen) {
-      if (max < arr_input[i]) {
-        max = arr_input[i];
+      if (max < arrInput[i]) {
+        max = arrInput[i];
         idx = i;
-      } else {
-        max = max;
-        idx = idx;
       }
-      i++; 
+      i++;
     }
     i = -1;
     while (i < arrLen) {
@@ -41,13 +37,11 @@ if (len_vec < 4) {
       if (i === idx) {
         continue;
       } else {
-        if (m2 < arr_input[i]) {
-          m2 = arr_input[i];
-        } else {
-          m2 = m2;
-          }
+        if (m2 < arrInput[i]) {
+          m2 = arrInput[i];
         }
       }
     }
+  }
   console.log(m2);
 }
