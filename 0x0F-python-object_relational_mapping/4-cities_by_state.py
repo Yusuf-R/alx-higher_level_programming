72823ce24101ba9319fac66010c012715c3d0fe5
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-22 06:19:37 +0100

    feat: :sparkles: a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -20,8 +20,10 @@ def main():
             port=pt
             )
     myDB_cursor = myDB.cursor()
-    myquery = ("SELECT * "
+    myquery = ("SELECT cities.id, cities.name, states.name "
                "FROM cities "
+               "JOIN states "
+               "ON cities.state_id = states.id "
                "ORDER BY cities.id ASC")
     try:
         myDB_cursor.execute(myquery)
