e9f204214eb0f316e9edf0ca4c0d010c6f49271d
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-23 03:39:18 +0100

    feat: :sparkles: set the file to act as database creator

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,35 +3,15 @@
 This module contains the base defination for using
 sqlalchemy
 """
-from sys import argv
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
+Base = declarative_base()
 
-if __name__ == "__main__":
-    """
-    Ensuring non spilling during import
-    """
 
-    Base = declarative_base()
-    """This is the base class for the declarative"""
-
-    usr = argv[1]
-    pswd = argv[2]
-    db = argv[3]
-    host = "localhost"
-    pt = 3306
-    query = "mysql+mysqldb://{}:{}@{}/{}".format(usr, pswd, host, db)
-
-    engine = create_engine(query, echo=True)
-    engine.connect()
-
-    class State(Base):
-        """ State class """
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True,
-                    autoincrement=True, nullable=False, unique=True)
-        name = Column(String(128), nullable=False)
-
-    Base.metadata.create_all(engine)
+class State(Base):
+    """ State class """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                autoincrement=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
