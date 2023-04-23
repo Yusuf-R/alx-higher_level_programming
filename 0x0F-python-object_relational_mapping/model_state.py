fb0369892e32002889cd94aca72a086ecdc4fddb
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-23 02:51:15 +0100

    feat: :sparkles: using sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
-"""State class module"""
-from sqlalchemy import Column, Integer, String
+"""
+This module contains the base defination for using
+sqlalchemy
+"""
+from sys import argv
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+usr = argv[1]
+pswd = argv[2]
+db = argv[3]
+host = "localhost"
+pt = 3306
 
-class State(Base):
-    """State class"""
-    __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://usr:pswd@host:pt/db')
+    engine.connect()
+
+    class State(Base):
+        __tablename__ = 'states'
+        id = Column(Integer, primary_key=True,
+                    autoincrement=True, nullable=False, unique=True)
+        name = Column(String(128), nullable=False)
+
+    Base.metadata.create_all(engine)
