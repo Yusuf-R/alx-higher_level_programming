638b7186b8fd35399b8be343e3e44ea666866351
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-24 11:06:40 +0100

    fix: :bug: added a session close

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,19 +6,20 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sys import argv
 
-usr = argv[1]
-pswd = argv[2]
-db = argv[3]
-
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                       .format(usr, pswd, db), pool_pre_ping=True)
-engine.connect()
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-
 if __name__ == "__main__":
+    usr = argv[1]
+    pswd = argv[2]
+    db = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(usr, pswd, db), pool_pre_ping=True)
+    engine.connect()
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
     result = session.query(State).order_by(State.id.asc()).all()
     for row in result:
         print("{}: {}" .format(row.id, row.name))
+    session.close()
