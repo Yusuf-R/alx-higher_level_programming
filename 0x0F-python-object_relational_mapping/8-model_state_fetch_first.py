f1118d0557ef67f0d351e03e1c8680cf6465731e
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-24 18:04:43 +0100

    feat: :sparkles: print out the first matching case for id == 1

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-"""Fetch state from database
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+"""module will use sqlalchemy to query of database table"""
 
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
+    usr = argv[1]
+    pswd = argv[2]
+    db = argv[3]
 
-    state = session.query(State).order_by(State.id).first()
-    if state:
-        print("{}: {}".format(state.id, state.name))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(usr, pswd, db), pool_pre_ping=True)
+    engine.connect()
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).where(State.id == 1).first()
+    if not result:
+        print("Nothing")
     else:
-        print('Nothing')
+        print("{}: {}" .format(result.id, result.name))
     session.close()
