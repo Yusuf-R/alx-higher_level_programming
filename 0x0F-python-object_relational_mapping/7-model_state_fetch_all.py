969f883edb9fea02e4cd3d288dab2140bd26d2b8
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-24 10:45:50 +0100

    feat: :sparkles: using sqlalchemy to querey all the states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Fetch state from database
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+
+from model_state import State, Base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+usr = argv[1]
+pswd = argv[2]
+db = argv[3]
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                       .format(usr, pswd, db), pool_pre_ping=True)
+engine.connect()
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+    result = session.query(State).order_by(State.id.asc()).all()
+    for row in result:
+        print("{}: {}" .format(row.id, row.name))
