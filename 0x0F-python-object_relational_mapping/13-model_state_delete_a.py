52061d8ab6dd38f0a3ba4cff3dd766640047b7fa
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-25 03:49:47 +0100

    feat: :sparkles: deleting the return of a query in the database

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-"""Fetch state from database
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+"""module will use sqlalchemy to query of database table"""
 
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    usr = argv[1]
+    pswd = argv[2]
+    db = argv[3]
+    letter = "a"
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(usr, pswd, db), pool_pre_ping=True)
+    engine.connect()
     Base.metadata.create_all(engine)
 
-    session = sessionmaker(bind=engine)()
-    states = session.query(State).filter(State.name.like('%a%')).all()
-    for state in states:
-        session.delete(state)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter(State.name.like('%:letter%')).all()
+    session.delete(query)
     session.commit()
     session.close()
