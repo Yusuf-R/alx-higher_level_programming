5546088ddd6a4afd4839bf1c899d655efac3314d
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-28 05:21:03 +0100

    fix: :bug: shell refactored

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script that sends a DELETE request to the URL passed
-# as the first argument and displays the body of the response
+# script that sends a DELETE request to the URL passedas the first argument and displays the body of the response
 curl -sI $1 | grep Allow | cut -d ' ' -f2-
