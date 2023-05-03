08b3b58092e1f907b49e8d96a524607e35f8b657
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-03 11:17:45 +0100

    feat: :sparkles: script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s -H "X-School-User-Id:98" "$1"
+# script that takes in a URL and displays the body of the response
+curl -s -H "X-School-User-Id: 98" "$1"
