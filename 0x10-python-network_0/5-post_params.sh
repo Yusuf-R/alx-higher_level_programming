88f10125426121eff48cf78a68e275f524f32585
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-03 14:08:33 +0100

    feat: :sparkles: send a POST request to a URL with email and subject variables

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" $1
+#Send a POST request to a URL with email and subject variables
+curl -s -X POST -H "Content-Type: application/json" -d '{"email":"test@gmail.com","subject":"I will always be here for PLD"}' "$1"
