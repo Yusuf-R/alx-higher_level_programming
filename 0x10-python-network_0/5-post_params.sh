4f78c6c80ca5faa4e817971b3d4f51117014be99
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-04 00:51:26 +0100

    feat: :sparkles: made use of the -d flag

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Send a POST request to a URL with email and subject variables
-curl -s -X POST -H "Content-Type: application/json" -d '{"email":"test@gmail.com","subject":"I will always be here for PLD"}' "$1"
+curl -s -X POST -d 'email=test@gmail.com' -d 'subject=I will always be here for PLD' "$1" # & also works instead of repeating -d
