b65d1ac723e04e51c5d036c1a2008f3df1e28286
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-03 08:52:06 +0100

    feat: :sparkles: script that sends a DELETE request to the URL passed as the first argument and displays the body of the response

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends DELETE
-curl -sX 'DELETE' $1
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -s --request "DELETE" "$1" # you can also use -X flag in place of --request
