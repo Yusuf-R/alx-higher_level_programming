c277e642fb6cac5d48db20181f8892717cf83432
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-03 10:53:19 +0100

    feat: :sparkles: script that takes in a URL and displays all HTTP methods the server will accept.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a DELETE request to the URL passedas the first argument and displays the body of the response
-curl -sI $1 | grep Allow | cut -d ' ' -f2-
+# script that takes in a URL and displays all HTTP methods the server will accept.
+curl -s -i --request OPTIONS "$1" | grep -i "Allow:" | cut -d " " -f 2- # -X flags can also be used
