15075502f2a6a3523ea6661e0342a82a65fdaaf0
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-03 07:43:02 +0100

    fix: :bug: script should be strictly 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-err_msg="Usage: <./script.sh> URL"
-# Check if URL argument was provided
-if [ -z "$1" ]; then
-  echo "$err_msg"
-  exit 1
-fi
-
-# Send request and display size of body
 curl -s -w '%{size_download}\n' -o /dev/null "$1"
