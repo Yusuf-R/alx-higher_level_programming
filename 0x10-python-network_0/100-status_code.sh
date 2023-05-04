b67a8d716bcdd7994d898ebbe973b40bd4f5fbaa
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-04 01:44:25 +0100

    feat: :sparkles: display the http code of the server response

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -so /dev/null -w "%{http_code}" $1
+# display only the http_code of the response header
+curl -s -w '%{status_code}' -o /dev/null "$1"
