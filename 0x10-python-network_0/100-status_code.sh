8abe917be2c280772be0150490984e6cac9e2fd3
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-04 01:50:34 +0100

    fix: :bug: fixed the parmeter passed, it was misssing a %

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display only the http_code of the response header
-curl -s -w '%{status_code}' -o /dev/null "$1"
+# displays the status code of the http response
+curl -s -w '%{http_code}' -o /dev/null "$1"
