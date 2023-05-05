d0e7da2116c8cd41eb456bedb29ac8fcb4960bfd
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-05 11:28:47 +0100

    fix: :bug: correct file now in place

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Send a POST request to a URL with email and subject variables
-curl -s -X POST -d 'email=test@gmail.com' -d 'subject=I will always be here for PLD' "$1" # & also works instead of repeating -d
+# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+curl -s -X POST -H "Content-Type: application/json" -d "@$2" "$1"
