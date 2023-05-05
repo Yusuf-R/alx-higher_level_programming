2270ef94a7bdc5a2328e3b5ae9108bf257ec521a
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-05 18:13:23 +0100

    feat: :sparkles: customize the header and the body to get the request approved by the server

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  a catch if you can script
-curl -sL -X PUT -H "Origin:HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
+# a script that makes a request to 0.0.0.0:5000/catch_me
+curl -s --request PUT -H "Origin:HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
