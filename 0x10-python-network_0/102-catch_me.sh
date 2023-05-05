be5f8b7ed44c8e91e7e530c9dd637e0d82664c0d
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-05 18:33:41 +0100

    fix: :bug: implmented a redirect

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that makes a request to 0.0.0.0:5000/catch_me
-curl -s --request PUT -H "Origin:HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
+curl -sL --request PUT -H "Origin:HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
