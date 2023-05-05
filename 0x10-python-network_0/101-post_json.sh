e95ec7ddac3503b70eb3e86261a1439ae386927c
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-05 11:08:48 +0100

    feat: :sparkles: script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s -X POST -d @"$2" -H "Content-Type: application/json" "$1"
+#Send a POST request to a URL with email and subject variables
+curl -s -X POST -d 'email=test@gmail.com' -d 'subject=I will always be here for PLD' "$1" # & also works instead of repeating -d
