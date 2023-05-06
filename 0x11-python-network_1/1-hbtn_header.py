d6564f06a9e0639700bd819cc03a98bb7a862cdd
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-06 10:43:24 +0100

    fix: :bug: request is needed properly parse the argv[1]

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,9 @@
 """a python script that fetches a url"""
 import urllib.request
 from sys import argv
-url = argv[1]
+
+url_req = argv[1]
+url = urllib.request.Request(url_req)
 with urllib.request.urlopen(url) as response:
     html_header = response.headers
     content = html_header.get("X-Request-Id")
