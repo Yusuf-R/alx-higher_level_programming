cf46cac897100f4f065ec60d6e56fe9da472198d
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-06 23:37:12 +0100

    fix: :bug: added test condition

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""a python script that fetches a url"""
+"""a python script that displays the X-Request-Id"""
 import urllib.request
 from sys import argv
 
-url_req = argv[1]
-url = urllib.request.Request(url_req)
-with urllib.request.urlopen(url) as response:
-    html_header = response.headers
-    content = html_header.get("X-Request-Id")
-    print(content)
+if len(argv) > 1:
+    url_req = argv[1]
+    url = urllib.request.Request(url_req)
+    with urllib.request.urlopen(url) as html:
+        content = html.getheader("X-Request-Id")
+        print(content)
