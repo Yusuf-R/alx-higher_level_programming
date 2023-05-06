c7c8f30138686525dce34251a9a718b93ac6494a
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-06 08:38:54 +0100

    feat: :sparkles: script fetches a url and prints its content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" script that fetches https://alx-intranet.hbtn.io/status"""
-from urllib import request
-
-
-if __name__ == '__main__':
-    my_req = request.Request("https://intranet.hbtn.io/status")
-    with request.urlopen(req) as response:
-        read_obj = response.read()
-        stats = "Body response:\n\t- type: {}\n\t- content: {}"
-        stats += "\n\t- utf8 content: {}"
-        print(stats.format(type(read_obj), read_obj, read_obj.decode('utf-8')))
+"""a python script that fetches a url"""
+import urllib.request
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    content_utf = html.decode("utf-8")
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(content_utf))
