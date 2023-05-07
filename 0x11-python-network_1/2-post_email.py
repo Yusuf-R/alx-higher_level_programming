230f2a519edc06f898cff7b3bfb4a522112904cd
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-07 03:01:22 +0100

    feat: :sparkles: send a post request with some header fields

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
-a Python script that takes in a URL, sends a request to the URL 
-and displays the body of the response (decoded in utf-8).
-"""
-from urllib import request, parse
+"""a python script that displays the X-Request-Id"""
+import urllib.request
+import urllib.parse
 from sys import argv
 
-
 if len(argv) > 2:
-    email = {'email': argv[2]}
-    my_data = parse.urlencode(email).encode('ascii')
-    my_req = request.Request(argv[1], my_data)
-    with request.urlopen(my_req) as my_resp:
-        rv = my_resp.read()
-        print(rv.decode('utf-8'))
+    url_req = argv[1]
+    email = argv[2]
+    info = {"email" : email}
+
+    data = urllib.parse.urlencode(info).encode("ascii")
+    url = urllib.request.Request(url_req, data=data, method="POST")
+
+    with urllib.request.urlopen(url) as html:
+        html_body = html.read().decode("utf-8")
+        print(html_body)
