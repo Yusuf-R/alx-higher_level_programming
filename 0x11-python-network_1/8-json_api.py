af3a6363bd1b7ce0ccf6ef8d7d06b68005f94455
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-07 07:12:45 +0100

    feat: :sparkles: script that takes in a letter and sends a POST request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 """
-takes letter and sends a POST req to http://0.0.0.0:5000/search_user"""
+Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
 import requests
 from sys import argv
 
+if len(argv) < 2:
+    q = ""
+else:
+    q = argv[1]
 
-if __name__ == '__main__':
-    letter = {'q': argv[1][0] if len(argv) > 1 else ''}
-    rv = requests.post('http://0.0.0.0:5000/search_user', data=letter)
-    try:
-        response = rv.json()
-        if response:
-            print('[{}] {}'.format(response.get('id'), response.get('name')))
-        else:
-            print('No result')
-    except ValueError:
-        print('Not a valid JSON')
+url = "http://0.0.0.0:5000/search_user"
+payload = {"q": q}
+html = requests.post(url, data=payload)
+try:
+    html_json = html.json()
+    if html_json:
+        print("[{}] {}".format(html_json.get("id"), html_json.get("name")))
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
