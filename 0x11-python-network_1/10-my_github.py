d2afb9c11606a73bec71c74bf687b3ab66f3d125
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-07 18:53:12 +0100

    feat: :sparkles: using github api to fetch my id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-"""GitHub credentials API to display id"""
+"""
+script that takes username and PAT
+and displays the id of the user
+"""
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    usr = argv[1]
+    paswd = argv[2]
+    url = "https://api.github.com/users"
 
-if __name__ == '__main__':
-    rv = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    html = requests.post(url, auth=(usr, paswd))
     try:
-        print(rv.json().get('id'))
+        html_json = html.json()
+        print("{}".format(html_json.get("id")))
     except ValueError:
-        print('Not a valid JSON')
+        print("Not a valid JSON")
