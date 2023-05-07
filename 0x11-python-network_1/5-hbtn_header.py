4bdd7380341546826de0f6e73e94346df82a0371
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-07 04:58:17 +0100

    feat: :sparkles: display the value for the given header key using the requests module

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Write a Python script that takes in a URL, sends a request
-to the URL and displays the value of the variable
-"""
+"""displays the X-Request-Id using rquests module"""
 import requests
 from sys import argv
 
-
-if __name__ == '__main__':
-    rv = requests.get(argv[1])
-    print(rv.headers.get('x-Request-Id'))
+if len(argv) > 1:
+    url = argv[1]
+    with requests.get(url) as html:
+        content = html.headers.get("X-Request-Id")
+        print(content)
