9c21e9800110285f65ed0223771bb560e9aa0188
Yusuf-R <y.abdulwasiu@gmail.com>
2023-05-07 18:34:03 +0100

    fix: :bug: implement code prevention exexution during export

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """
 Python script that takes in a letter and sends a POST request
-to http://0.0.0.0:5000/search_user 
+to http://0.0.0.0:5000/search_user
 """
 import requests
 from sys import argv
 
-if len(argv) < 2:
-    q = ""
-else:
-    q = argv[1]
-
-url = "http://0.0.0.0:5000/search_user"
-payload = {"q": q}
-html = requests.post(url, data=payload)
-try:
-    html_json = html.json()
-    if html_json:
-        print("[{}] {}".format(html_json.get("id"), html_json.get("name")))
+if __name__ == "__main__":
+    if len(argv) < 2:
+        q = ""
     else:
-        print("No result")
-except ValueError:
-    print("Not a valid JSON")
+        q = argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {"q": q}
+    html = requests.post(url, data=payload)
+    try:
+        html_json = html.json()
+        if html_json:
+            print("[{}] {}".format(html_json.get("id"), html_json.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
