d47f670df4f7721520342e446160e4338f9a339c
Yusuf-R <y.abdulwasiu@gmail.com>
2023-07-26 21:32:01 +0100

    feat: :sparkles: print the title of star wars movie API reply

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,25 @@
 #!/usr/bin/node
-//js script to prints the title of a starwars movie
+// a script to print the title of a Star Wars movie
 const request = require('request');
+const usrId = process.argv[2];
 
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (err, response, body) {
-  console.log(err || JSON.parse(body).title);
+const id = parseInt(usrId, 10);
+
+if (isNaN(id)) {
+  console.log('Error in id: id must be int');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const moveiData = JSON.parse(body);
+    console.log(moveiData.title);
+  } else {
+    console.log('Error in data:', response.statusCode);
+    process.exit(1);
+  }
 });
