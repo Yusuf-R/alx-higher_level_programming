2a50875b7e7c3d2f38b59f2635509e853aa648fa
Yusuf-R <y.abdulwasiu@gmail.com>
2023-07-26 18:53:06 +0100

    fix: :bug: semi-standard compliant

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,8 +2,8 @@
 // a script to read the content of a file
 const fs = require('fs');
 const file = process.argv[2];
-const string_to_write = process.argv[3];
-fs.writeFile(file, string_to_write, 'utf-8', (err) => {
+const stringWrite = process.argv[3];
+fs.writeFile(file, stringWrite, 'utf-8', (err) => {
   if (err) {
     return console.log(err);
   }
