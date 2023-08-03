4a934e204b631e6b09660cd5f7de6686b46a45f9
Yusuf-R <y.abdulwasiu@gmail.com>
2023-08-03 02:17:08 +0100

    feat: :sparkles: no jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000):
+// You must use document.querySelector to select the HTML tag
+// You can’t use the jQuery API
+// Note: Your script must be imported from the <head> tag, not at the end of the HTML
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('HEADER').style.color = '#FF0000';
+});
