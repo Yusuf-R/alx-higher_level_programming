e563cc06d6f5ba5de61362ef55c96177c1770050
Yusuf-R <y.abdulwasiu@gmail.com>
2023-08-05 22:56:41 +0100

    feat: :sparkles: maipulating DOM elements

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,9 +1,7 @@
 // Adds a <li> element to a list when the user clicks on tag `DIV#add_item`
-
-const htmlDoc = $(document);
-const item = '<li>Item</li>';
-const ulClass = $('UL.my_list');
-const addItemDiv = $('DIV#add_item');
-const addAnItem = () => ulClass.append(item);
-
-htmlDoc.ready(addItemDiv.on('click', addAnItem));
+$('document').ready(function () {
+  $('#add_item').on('click', function () {
+    const item = '<li>Item</li>';
+    $('.my_list').append(item);
+  });
+});
