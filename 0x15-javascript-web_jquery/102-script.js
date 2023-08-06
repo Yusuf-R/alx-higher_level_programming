637c52a3975c9276eb7aa29ca98e86de50c0b528
Yusuf-R <y.abdulwasiu@gmail.com>
2023-08-06 19:06:11 +0100

    feat: :sparkles: say hello to everybody!

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,8 +1,15 @@
 // script that fetches and prints how to say “Hello” depending on the language
-$('document').ready(() => {
-  $('INPUT#btn_translate').click(() => {
-    $.get('https://www.fourtonfish.com/hellosalut/?lang=' + $('INPUT#language_code').val(), (data) => {
-      $('DIV#hello').text(data.hello);
+const url = 'https://hellosalut.stefanbohacek.dev/';
+$(document).ready(function () {
+  $('#btn_translate').on('click', function () {
+    let cnt = $('#language_code').val();
+    if (cnt === '') {
+      cnt = 'en';
+    }
+    const urlMod = url + '?lang=' + cnt;
+    $.get(urlMod, function (data) {
+      const msg = data.hello;
+      $('#hello').text(msg);
     });
   });
 });
