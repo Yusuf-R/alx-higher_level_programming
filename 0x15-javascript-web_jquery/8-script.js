8c4b1befe65bdfd2cb9688805c3e13279035a6d4
Yusuf-R <y.abdulwasiu@gmail.com>
2023-08-06 02:57:22 +0100

    feat: :sparkles: using api to fetch an dynamically update our html front

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,9 +1,20 @@
 // Fetches and list title of all movies from URL
+url = 'https://swapi-api.alx-tools.com/api/films/?format=json'
+$.get(url, function (data) {
+  let i = 0;
+  if (data.results.length === 0) {
+    $('#list_movies').empty();
+    return
+  }
+  else {
+    let msg = data.results;
+    while(i < msg.length) {
+      $('#list_movies').append(`<li>${msg[i].title}</li>`);
+      i++;
+      //$('#list_movies').append(msg[i].title + '<br />');
+      //console.log(msg[i]);
+      //i++;
+    }
+  }
 
-const htmlDoc = $(document);
-const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
-const htmlTag = 'UL#list_movies';
-const addToList = (_i, movie) => $('<li>').text(movie.title).appendTo(htmlTag);
-const listMovies = (data) => $.each(data.results, addToList);
-
-htmlDoc.ready($.getJSON(url, listMovies));
+});
