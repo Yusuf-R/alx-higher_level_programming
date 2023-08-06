e128821735e301948b0270615b2f6cf10a341ac0
Yusuf-R <y.abdulwasiu@gmail.com>
2023-08-06 01:00:41 +0100

    feat: :sparkles: update header with a new text upon clicking

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,8 +1,7 @@
 // Update text of <header> when user clicks `DIV#update_header`
-
-const htmlDoc = $(document);
-const updateDiv = $('DIV#update_header');
-const userText = 'New Header!!!';
-const updateHeaderText = () => $('header').text(userText);
-
-htmlDoc.ready(updateDiv.on('click', updateHeaderText));
+$('document').ready(function () {
+  $('#update_header').on('click', function () {
+    const msg = 'New Header!!!';
+    $('header').text(msg);
+  })
+})
